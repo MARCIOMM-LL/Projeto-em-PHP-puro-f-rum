bb562adacc359273d2155d95da58b94ce0de0a16
Márcio <32685719+MARCIOMM-LL@users.noreply.github.com>
2020-06-21 14:17:08 -0300

    Update signup.php

--- signup.php
+++ signup.php
@@ -24,15 +24,15 @@
     //Apresenta o formulário para adiçionar novo usuário
     echo '
            <form class="form_signup" method="post" action="signup.php?a=signup" enctype="multipart/form-data">
-           <h3>Signup</h3><hr><br>
-           Username:<br><input type="text" size="20" name="text_utilizador"><br><br>
-           Password:<br><input type="password" size="20" name="text_password_1"><br><br>
-           Re-escrever password:<br><input type="password" size="20" name="text_password_2"><br><br>
-           <input type="hidden" name="MAX_FILE_SIZE" value="50000">
-           Avatar:<input type="file" name="imagem_avatar"><br>
-           <small>[Imagem do tipo <strong>JPG</strong>, tamanho máximo: <strong>50Kb</strong>]</small><br><br>
-           <input type="submit" name="btn_submit" value="Registrar"><br><br>
-           <a href="index.php">Voltar</a>
+              <h3>Signup</h3><hr><br>
+              Username:<br><input type="text" size="20" name="text_utilizador"><br><br>
+              Password:<br><input type="password" size="20" name="text_password_1"><br><br>
+              Re-escrever password:<br><input type="password" size="20" name="text_password_2"><br><br>
+              <input type="hidden" name="MAX_FILE_SIZE" value="50000">
+              Avatar:<input type="file" name="imagem_avatar"><br>
+              <small>[Imagem do tipo <strong>JPG</strong>, tamanho máximo: <strong>50Kb</strong>]</small><br><br>
+              <input type="submit" name="btn_submit" value="Registrar"><br><br>
+              <a href="index.php">Voltar</a>
            </form>
          '; 
   }
